f53167885d17253066d2b85f6b8be646820a9988
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-01-28 06:11:24 +0300

    Create VOTE.py

---
+++ VOTE.py
@@ -0,0 +1,12 @@
+def avg(data):
+    marks = [int(x) for x in data.split()]
+    avg = sum(marks)/len(marks)
+    return avg
+
+def main():
+    judge = input('Оценки судей (через пробел):')
+    people = input('Оценки народного голосования (через пробел):')
+    mark = avg(judge) + avg(people)
+    print(f'Итоговая оценка: {mark}')
+
+main()
