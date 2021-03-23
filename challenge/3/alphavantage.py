fe989c46ffc8b58ad136f7a4198dc1c453704f23
Alexander Nazarovsky <ANazarovsky@gmail.com>
2021-03-23 18:40:23 +0300

    AlphaVantage API

--- challenge/3/alphavantage.py
+++ challenge/3/alphavantage.py
@@ -1,41 +1,73 @@
 import pyfiglet
+import os
 import requests
+import pandas as pd
+
 
 def upload(func, ticker, data):
    
-    extensions = {
-        "O": "-overview",
-        "T": "-ts",
-        "E": "-earnings",
-        "I": "-income"
+    file_name = {
+        "O": "overview",
+        "T": "time_series",
+        "E": "earnings",
+        "I": "income"
     }
 
-    with open(f"alphavantage_data/{ticker}{extensions.get(func)}.csv", "w") as fin:
+    with open(f"data/{ticker}/{file_name.get(func)}.csv", "w") as fin:
         fin.write(data)
 
 
-print(pyfiglet.figlet_format("AlphaVantage API", font="smslant"))
-apikey = "TLKJ1KPHYFL689BM"
-url = "https://www.alphavantage.co/query"
-symbol = input("Tiker: ").upper()
-query = input("[O]verview\n[T]ime series\n[E]arnings\n\
+def main():
+
+    print(pyfiglet.figlet_format("AlphaVantage API", font="smslant"))
+    apikey = "TLKJ1KPHYFL689BM"
+    url = "https://www.alphavantage.co/query"
+    params = {"function": "LISTING_STATUS", "apikey": apikey}
+    update = input("Update the listing? Y/N\n").upper()
+    if update == "Y":
+        with open ("list.csv", "w") as fin:
+            fin.write(requests.get(url, params=params).text)
+    elif update == "N":
+        pass
+    df = pd.read_csv("list.csv", index_col="symbol")
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    while True:
+        symbol = input("Tiker: ").upper()
+        if symbol not in df.index:
+            print("No info about given ticker.")
+            continue
+        if not os.path.exists(os.path.join("data", symbol)):
+            os.mkdir(os.path.join("data", symbol))
+        query = input("[O]verview\n[T]ime series\n[E]arnings\n\
 [I]income\n").upper()
 
-fuctions = {
-    "O": "OVERVIEW",
-    "T": "TIME_SERIES_DAILY",
-    "E": "EARNINGS",
-    "I": "INCOME_STATEMENT"
-}
+        if query == "Q":
+            break
 
-params = {
-    "function": fuctions.get(query),
-    "symbol": symbol,
-    "datatype": "csv",
-    "apikey": apikey
-}
+        functions = {
+            "O": "OVERVIEW",
+            "T": "TIME_SERIES_DAILY",
+            "E": "EARNINGS",
+            "I": "INCOME_STATEMENT"
+        }
 
-response = requests.get(url, params=params)
-data = response.text
-upload(query, symbol, data)
-print("upload complete")
+        params = {
+            "function": functions.get(query),
+            "symbol": symbol,
+            "datatype": "csv",
+            "apikey": apikey
+        }
+
+        response = requests.get(url, params=params)
+        if query not in functions.keys():
+            print("No such option. Please, check your query.")
+            continue
+        data = response.text
+        upload(query, symbol, data)
+        print("upload complete")
+
+
+if __name__ == "__main__":
+    main()
